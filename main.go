302daa4a793c8c3842bbde238637fb33a526fc2b
YannLebouc <yanlpi@proton.me>
2026-02-28 22:12:51 +0100

    feat: Reading into feeds config file where RSS URLs will live

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"bufio"
+	"log"
 )
 
 func main(){
-	fmt.Println("Hello World.")
+	// fmt.Println("Hello World.")
+    log.SetPrefix("file reader: ")
+    log.SetFlags(0)
+
+	file, err := os.Open("/home/ylebouc/dotfiles/rss-tui/feeds")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
