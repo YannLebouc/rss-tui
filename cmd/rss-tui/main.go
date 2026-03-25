9e08ad5223c98b2eea8c028eb10f6a94845adbfc
YannLebouc <yanlpi@proton.me>
2026-03-25 14:16:12 +0100

    feat: Added Fetcher struct, containing an http client to use as an engine for future request

--- cmd/rss-tui/main.go
+++ cmd/rss-tui/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
 	"log"
-	"net/http"
+
 	"github.com/YannLebouc/rss-tui/internal/config"
+	"github.com/YannLebouc/rss-tui/internal/fetch"
 )
 
 func main() {
@@ -19,22 +19,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i := range feeds {
-		response, err := http.Get(feeds[i].URL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode >= 300 {
-			log.Fatalf("Response failed with status code %d and body %s\n", response.StatusCode)
-		}
-
-		decoder := xml.NewDecoder(response.Body)
-		if err := decoder.Decode(&feeds[i]); err != nil {
-			log.Fatalf("Failed to decode feed from URL %s: %v\n", feeds[i].URL, err)
-		}
+	fetcher := fetch.NewFetcher()
+	feed, err := fetcher.Fetch(feeds[0].URL)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Println(feeds[0].Channel)
+	fmt.Println(feed.Channel.Items[0])
 }
--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -1,1 +1,40 @@
 package fetch
+
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+
+	"github.com/YannLebouc/rss-tui/internal/rss"
+)
+
+type Fetcher struct {
+	httpClient *http.Client
+}
+
+func NewFetcher() *Fetcher {
+	return &Fetcher{
+		httpClient: &http.Client{},
+	}
+}
+
+func (f *Fetcher) Fetch(url string) (rss.Feed, error) {
+	response, err := f.httpClient.Get(url)
+	if err != nil {
+		return rss.Feed{}, err
+	}
+
+	defer response.Body.Close()
+
+	if !(response.StatusCode == 200) {
+		return rss.Feed{}, fmt.Errorf("HTTP reponse code expected : 200, got %d while trying to fetch %s", response.StatusCode, url)
+	}
+
+	feed := rss.Feed{}
+	decoder := xml.NewDecoder(response.Body)
+	if err := decoder.Decode(&feed); err != nil {
+		return rss.Feed{}, err
+	}
+
+	return feed, nil
+}
