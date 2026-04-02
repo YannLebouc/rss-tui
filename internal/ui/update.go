bf6b96b87c6564eaa745a4ce59e8c614d963731a
YannLebouc <yanlpi@proton.me>
2026-04-02 17:35:27 +0200

    feat: #WIP handling of cursor index depending on current model mode

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -25,13 +25,30 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		// The "down" and "j" keys move the cursor down
 		case "down", "j":
-			if m.cursor < len(m.feeds[0].Channel.Items)-1 {
-				m.cursor++
+			if m.mode == FEEDS_LIST {
+				if m.cursor < len(m.feeds)-1 {
+					m.cursor++
+				}
+			}
+			if m.mode == ARTICLES_LIST {
+				if m.cursor < len(m.feeds[m.selectedFeed].Channel.Items)-1 {
+					m.cursor++
+				}
 			}
 		// refreshing feeds
 		case "r":
 			m.loading = true
 			return m, LoadFeeds
+		case "enter":
+			if m.mode == FEEDS_LIST {
+				m.selectedFeed = m.cursor
+				m.cursor = 0
+				m.mode = ARTICLES_LIST
+			}
+			if m.mode == ARTICLES_LIST {
+				m.selectedArticle = m.cursor
+				m.mode = ARTICLE_DETAIL
+			}
 		}
 
 	case FeedsLoadedMsg:
